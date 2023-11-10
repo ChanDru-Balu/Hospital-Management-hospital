0f1f9941478481f4f5ab93e13eb0787647e9d089
ChanDru-Balu <prochandru@gmail.com>
2023-11-10 16:38:10 +0530

    initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'hospital',
+  appId: 'chan.hospital.hospital',
+  appName: 'HosMan Hospital',
   webDir: 'www',
   server: {
     androidScheme: 'https'
