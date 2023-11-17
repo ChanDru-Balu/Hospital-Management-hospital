9cebb5a95f6a6c70a19de6c90076e640faf7c4cc
ChanDru-Balu <prochandru@gmail.com>
2023-11-17 21:20:28 +0530

    Request Prompt

---
+++ android/app/src/main/java/chan/hospital/hospital/MainActivity.java
@@ -0,0 +1,5 @@
+package chan.hospital.hospital;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 
+import { LocalNotifications , ScheduleOptions  } from '@capacitor/local-notifications';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
@@ -12,6 +14,7 @@ export class ProfilePage implements OnInit {
   name: any;
   mobile: any;
   email: any;
+  ids: any = [];
 
   constructor(
     private locationStrategy: LocationStrategy ,
@@ -23,6 +26,14 @@ export class ProfilePage implements OnInit {
     this.name = localStorage.getItem('name')
     this.mobile = localStorage.getItem('mobile')
     this.email = localStorage.getItem('email')
+    console.log("Profile Page")
+    this.LocalNotification()
+    // setInterval(()=>{
+    // console.log("Interval")
+
+    //   this.schedule();
+    // },1000)
+
   }
 
   goBack(){
@@ -32,6 +43,39 @@ export class ProfilePage implements OnInit {
   logout(){
     localStorage.clear();
     this.navController.navigateRoot('login')
+  }
+
+
+  async LocalNotification(){
+    const prompt = await LocalNotifications.requestPermissions()
+    await this.schedule()
+    // const permissions : any = await LocalNotifications.checkPermissions();
+    // console.log({permissions})
+    // if (!permissions.granted) {
+    //   // Local notifications are not enabled, prompt the user
+    // } else {
+    //   const prompt = await LocalNotifications.requestPermissions()
+    //   console.log({prompt})
+    // }
+  }
+
+
+  schedule(){
+    console.log("Schedule")
+    var t = new Date();
+    t.setSeconds(t.getSeconds() + 5);
+    let id = this.ids.length;
+    this.ids.push(id);
+
+    let options : ScheduleOptions = { notifications: [{
+      id: id,
+      title: "Local Notification id = " +id,
+      body: "Local Notification Body"
+    }] }
+
+    LocalNotifications.schedule(options).then((res)=>{
+      console.log({res})
+    })
   }
 
 }
